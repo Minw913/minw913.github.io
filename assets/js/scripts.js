b345bb7c9bf8e5629d983ebb07369ce441534bc5
BBBecky0913 <minw_k@hotmail.com>
2024-10-07 14:42:02 +0000

    minor

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,17 +1,17 @@
 document.addEventListener('DOMContentLoaded', function () {
-    // 获取按钮
     const toggleButton = document.getElementById('dark-mode-toggle');
   
-    // 检查 localStorage 中是否保存了用户的偏好设置
+    // 检查 localStorage 中是否有用户的偏好设置
     if (localStorage.getItem('darkMode') === 'enabled') {
-      document.body.classList.add('dark-mode'); // 如果是黑夜模式，则直接应用
+      document.body.classList.add('dark-mode');
+      toggleButton.checked = true; // 如果黑夜模式已开启，复选框应该被勾选
     }
   
-    // 点击按钮时切换黑夜模式
+    // 监听按钮的点击事件
     toggleButton.addEventListener('click', function () {
       document.body.classList.toggle('dark-mode'); // 切换 dark-mode 类
   
-      // 根据当前模式保存用户偏好到 localStorage
+      // 保存用户的偏好设置到 localStorage
       if (document.body.classList.contains('dark-mode')) {
         localStorage.setItem('darkMode', 'enabled');
       } else {
